b6f463714bb1097ecaf865642768c8b1dccc788d
David Tonhofer <ronerycoder@gluino.name>
2021-02-17 17:31:53 +0100

    Added code for texty typetests

--- unpacked/onepointfour_strings/stringy.pl
+++ unpacked/onepointfour_strings/stringy.pl
@@ -1,5 +1,6 @@
 :- module(onepointfour_strings_stringy,
           [
+          
           stringy/1              % is the argument an atom or a string or a number
          ,stringy_is_empty/1     % is the argument the empty atom,string or list
          ,stringy_is_chars/1     % is the argument a proper list of chars
@@ -23,6 +24,7 @@
          ,stringy_ensure/3       % string_ensure(Sin,Sout,Want)
          % Determine the "stringy length" of a thing "Sin"
          ,stringy_length/2  % string_length(Sin,Length)
+         
          ]).
 
 :- use_module(library('onepointfour_basics/meta_helpers.pl')).
@@ -46,173 +48,10 @@ If you have loaded this module, a call to load_test_files/1 should run the tests
 
 - 2021-01-19: Reviewed
 - 2021-02-16: Reviewed for packs, updated to pldoc.
+- 2021-02-17: Trashed all the old code for testing "stringyness" and wrote new one based on the "texty type tree"
+
 */
 
-%! stringy(+X) Stringy type test.
-%
-% Succeeds if X is something "stringy", fails otherwise. An unbound variables
-% leads to failure. Stringy things are:
-%
-%    - atoms
-%    - SWI-Prolog strings 
-%    - a number is accepted too, as it is "transformable into a string" by
-%      atom_string/2 (which leads to the question: "how exactly
-%      is a float or a rational transformed into a string?". The answer is:
-%      it depends!
-%
-% Compare with must_be/2 which knows of the following types:
-%
-%    - chars  : list of 1-character atoms; includes the empty list
-%    - codes  : list of integers >= 0; includes the empty list
-%    - string : passes string/1, an SWI-Prolog string
-%    - atom   : passes atom/1
-%    - text   : atom or string or chars or codes (but not numbers even though some predicates "textify" those)
-%
-% @bug It is difficult to judge whether numbers or event lists of chars or lists of numbers should be in this set.
-
-stringy(X) :-
-   (atom(X)
-    ;
-    string(X)
-    ;
-    number(X)),!. % terminal ! to make this deterministic
-
-
-%! stringy_is_empty(+X) Succeed exactly if X is "empty": The empty string, the empty atom but also the empty list.
-% 
-% @bug It is difficult to judge whether the empty list should be in this set. If the empty list is in here,
-% should stringy/1 succeed for lists?
-
-stringy_is_empty(X) :- 
-   (X==''
-    ;
-    X==""
-    ;
-    X==[]),!.  % terminal ! to make this deterministic
-
-IS: Certainly is
-IS_NOT: Certainly is not
-CAN_BE: Looks like but future instantiation can still change this
-Attribute: EMPTY
-
-                                                                                  
-
-                                               +---- openvarlist?: nonempty open list of >= 1 var
-                                               |                                       
-                             +--- openlist? ---+---- opencharlist?: nonempty open list of >= 1 char, may contain vars
-                             |                 |                                       
-                             |                 +---- opencodelist?: nonempty open list of >= 1 unicode code points, may contain vars
-                             |
-                             |                                                         +--- charlist! nonempty list of only chars ("characters", atoms of length 1)
-                             |                                                         |
-                             |                                       +---- charlist ---+
-                             |                                       |                 |   
-                             |                                       |                 +--- charlist? nonempty list of at least 1 char and at least 1 var
-                             |                                       |
-                             |                                       |                 +--- codelist! nonempty list of only unicode code points
-                             |                                       |                 |
-                             |                                       +---- codelist ---+
-                             |                                       |                 |   
-                             |                                       |                 +--- codelist? nonempty list of at least 1 unicode code points and at least 1 var
-                             |                       +--- list ------+                                                                                  
-                             |                       |               |          
-                             |                       |               +---- varlist?: nonempty list of only vars; edge case
-                             |                       |               |
-                             |                       |               +---- emptylist?: empty list; edge case
-                   texty? ---+             +---text--+           
-                             |             |         |          
-                             |             |         |               +-- atom! (including the empty atom)          
-                             |             |         |               |          
-                             +---anytext---+         +--- stringy! --+
-                             |             |                         |          
-                             |             |                         +-- string! (SWI-Prolog strings including the empty string)          
-                             |             |          
-                             |             +-- number!: acceptable because a number can be           
-                             |                          transformed into text (according to some           
-                             |                          unspecified convention...)
-                             |                       
-                             +--- var? (could be anything)
-
-success
- settled : it is texty and will stay texty (actually, it is in the category and will stay in the category)
- free:     it is text now but may become nontexty depending on future instantiations
-failure
- it's not texty
-
-attributes
- 
- empty     if it is a closed list, it is empty, if it is an atom, it is the empty atom, if it is a string, it is the empty string
- length(N) if it is a list, it has length N (even for an open list)
- vars(N)   if it is a list, it has N vars
- open      if it is a list, it's an open list
- closed    if it is a list, it's a closed list
- 
-category
-   
-%! textycat(@X,?Cat,?Surety,?Attributes)
-
-textycat(X,C) :- nonvar(C), textycat(X,CC),isa(CC,C).
-
-textycat(X,var)    :- var(X),!.
-textycat(X,string) :- string(X),!.
-textycat(X,atom)   :- atom(X),!.
-
-validcat(C) :-
-
-
-isa(SubCat,SuperCat) :- parent(SubCat,SuperCat).
-isa(SubCat,SuperCat) :- parent(SubCat,NextCat),isa(NextCat,SuperCat).
-
-parent(var,texty).
-parent(anytext,texty).
-parent(openlist,texty).
-parent(number,anytext).
-parent(text,anytext).
-parent(list,text).
-parent(stringy,text).
-parent(atom,stringy).
-parent(string,stringy).
-parent(charlist,list).
-parent(codelist,list).
-parent(varlist,list).
-parent(emptylist,list).
-parent(openvarlist,openlist).
-parent(opencharlist,openlist).
-parent(opencodelist,openlist).
-
-canbe(var).
-canbe(varlist).
-canbe(emptylist).
-canbe(openlist).
-
-
-
-
-
-
-
-
-attributes: canbe, empty, vars(N) in lists length(N) in lists
-
-any-var   -> can be
-any-anytext-number
-any-anytext-text-stringy-atom  maybe empty
-any-anytext-text-stringy-string  maybe empty
-any-anytext-text-list-chars      maybe can be if there are some vars
-any-anytext-text-list-codes      maybe can be if there are some vars
-any-anytext-text-list-vars       -> can be
-any-anytext-text-list-empty      -> can be
-any-anytext-openlist-vars        -> can be
-any-anytext-openlist-chars       -> can be
-any-anytext-openlist-codes       -> can be
-
-relationship compatible with 
-anytext
-any
-stringy
-text
-list
-
 
 
 
@@ -228,73 +67,7 @@ list
 
 
 
-          
-%! stringy_is_anytext(X)
-%
-% Succeed if X is "anytext" (which is stuff that can be processed by
-% SWI-Prolog string manipulation predicates): atom or string or chars or codes or number.
-% The string_is_anytext/2 reifies the type: 'atom', 'string', 'number', 'chars', 'codes', 'empty'
-% ===
 
-stringy_is_anytext(X) :-
-   stringy_is_anytext(X,_).
-
-stringy_is_anytext(X,What) :-
-   switch(
-      X==[]               , (What=empty), % inherently ambiguous
-      atom(X)             , (What=atom), % no special distinction for the empty atom
-      string(X)           , (What=string), % no special distinction for the empty string
-      number(X)           , (What=number),
-      stringy_is_chars(X) , (What=chars),
-      stringy_is_codes(X) , (What=codes),
-      fail),!.  % terminal ! to make this deterministic
-
-% ===
-% Succeed if X is something "text stringy" (the understanding for "text" from must_be/2)
-% text : atom or string or chars or codes (but not numbers)
-% ===
-
-stringy_is_text(X) :-
-   (stringy_is_atomic(X)
-    ;
-    stringy_is_chars(X)
-    ;
-    stringy_is_codes(X)),!. % terminal ! to make this deterministic
-
-% ===
-% Succeed if X is something "atomic stringy":
-% - an atom
-% - a string
-% ===
-
-stringy_is_atomic(X) :-
-   (atom(X)
-    ;
-    string(X)),!. % terminal ! to make this deterministic
-
-% ===
-% Succeed if X is a list of chars. We zip down the whole list.
-% This does not accept unbound variables and open lists.
-% ===
-
-stringy_is_chars(X) :-
-   switch(
-      var(X),    fail,
-      X==[],     true,
-      X=[C|Cs],  (atom(C),atom_length(C,1),stringy_is_chars(Cs)),
-      fail).
-
-% ===
-% Succeed if X is a list of codes. We zip down the whole list.
-% This does not accept unbound variables and open lists.
-% ===
-
-stringy_is_codes(X) :-
-   switch(
-      var(X),    fail,
-      X==[],     true,
-      X=[C|Cs],  (integer(C),between(0,0x10FFFF,C),stringy_is_codes(Cs)),
-      fail).
 
 % ===
 % Transform a stringy thing (actually, a less-than-stringy thing: only atoms or
@@ -390,6 +163,10 @@ stringy_concat_pair_to_string(Sin1,Sin2,Sout) :-
    unless(nonvar(Sin2),instantiation_error(_)),  % 'Sin2' must be a given. TODO: Fix exception
    string_concat(Sin1,Sin2,Sout).                % this transforms arbitrary transformations on Sin2, but yields a string
 
+   
+   
+   
+   
 % ===
 % Transform a stringy thing "Sin" into the stringy thing "Sout", which shall have
 % the type given by "Want": either atom or string.
@@ -421,10 +198,10 @@ stringy_ensure(Sin,Sout,Want) :-
       domain_error(_,_)).                       % TODO: Fix exception
 
 
-%! stringy_length(+Sin:Text,?Length:Integer)
+%! stringy_length(+Stringy:Stringy,?Length:Integer)
 %
-% Determine the length of a _stringy thing_ Sin, which may be an atom,
-% a string or some round term that can be transformed into something that
+% Determine the length of a _stringy thing_ Stringy, which may be an atom
+% or a string. or some round term that can be transformed into something that
 % has a "length".
 %
 % This is not really needed a atom_length/2 and string_length/2 work for
