7cef0f96418b60cc675350403fd45e91c2247573
David Tonhofer <ronerycoder@gluino.name>
2021-02-24 00:50:08 +0100

    Rearranging

---
+++ prepacked/onepointfour/basics/pack.pl
@@ -0,0 +1,7 @@
+% https://eu.swi-prolog.org/howto/PackInfo.html
+
+name(onepointfour_basics).
+title('Basic elements'.
+version('1.0.2').
+author('David Tonhofer','ronerycoder@gluino.name').
+maintainer('David Tonhofer','ronerycoder@gluino.name').
--- prepacked/onepointfour_basics/pack.pl
+++
@@ -1,4 +0,0 @@
-name(onepointfour_basics).
-title('Basic elements').
-version('1.0.1').
-author('David Tonhofer','ronerycoder@gluino.name').
