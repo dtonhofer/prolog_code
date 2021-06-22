99af936b06421e0caa4fde3938241250cea97430
David Tonhofer <ronerycoder@gluino.name>
2021-06-23 00:22:39 +0200

    Update topmost.pl

--- unpacked/onepointfour_basics/dict_pp/topmost.pl
+++ unpacked/onepointfour_basics/dict_pp/topmost.pl
@@ -28,8 +28,10 @@
     publish, distribute, sublicense, and/or sell copies of the Software,
     and to permit persons to whom the Software is furnished to do so,
     subject to the following conditions:
+
     The above copyright notice and this permission notice shall be
     included in all copies or substantial portions of the Software.
+
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -43,6 +45,11 @@
 
 This module exports the predicate which is called from dict_pp/3 and
 eventually from itself if dict prettyprinting needs to prettyprint subdicts.
+
+The homepage for this module is at
+
+https://github.com/dtonhofer/prolog_code/blob/main/unpacked/onepointfour_basics/README_dict_pp.md
+
 */
 
 %! pp_if_shallow_enough(+Dict,+SettingsDict,-LinesOut)
