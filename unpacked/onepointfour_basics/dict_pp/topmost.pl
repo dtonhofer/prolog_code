1a6e24fe37d685dfb8e256d602345c9d3e52d9dd
David Tonhofer <ronerycoder@gluino.name>
2021-06-23 00:19:35 +0200

    Update topmost.pl

--- unpacked/onepointfour_basics/dict_pp/topmost.pl
+++ unpacked/onepointfour_basics/dict_pp/topmost.pl
@@ -1,16 +1,7 @@
 :- module(onepointfour_basics_dict_pp_topmost,
           [
-          pp_if_shallow_enough/3 % pp_if_shallow_enough(Dict,SettingsDict,LinesOut)
+          pp_if_shallow_enough/3   % pp_if_shallow_enough(Dict,SettingsDict,LinesOut)
           ]).
-
-/** <module> Recursively called predicate for dict prettyprinting
-
-This module exports the predicate which is called from dict_pp/3 and
-eventually from itself if prettyprinting needs to prettyprint subdicts.
-
-*/
-
-% :- use_module(library('onepointfour_basics/dict_pp/pairs_to_entries.pl')).
 
 :- use_module(library(yall)).            % yall meta predicates (slow)
 :- use_module(library(apply)).           % meta-call predicates (slow)
@@ -25,6 +16,34 @@ eventually from itself if prettyprinting needs to prettyprint subdicts.
 :- use_module(library('onepointfour_basics/dict_pp/lineify.pl')).
 :- use_module(library('onepointfour_basics/dict_pp/helpers.pl')).
 :- use_module(library('onepointfour_basics/dict_pp/string_stuff.pl')).
+
+/*  MIT License Follows (https://opensource.org/licenses/MIT)
+
+    Copyright 2021 David Tonhofer <ronerycoder@gluino.name>
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files
+    (the "Software"), to deal in the Software without restriction,
+    including without limitation the rights to use, copy, modify, merge,
+    publish, distribute, sublicense, and/or sell copies of the Software,
+    and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/** <module> Recursively called predicate for dict prettyprinting
+
+This module exports the predicate which is called from dict_pp/3 and
+eventually from itself if dict prettyprinting needs to prettyprint subdicts.
+*/
 
 %! pp_if_shallow_enough(+Dict,+SettingsDict,-LinesOut)
 %
@@ -51,7 +70,7 @@ dict_to_string(Dict,Line) :-
 % depending on what SettingsDict says. Whether the dict's tag is printed
 % depends on whether it is set and on what SettingsDict says about that.
 
-dict_pp_inner(Dict,SettingsDict,LinesOut) :-
+dict_pp_inner(Dict,SettingsDict,ResultLines) :-
    %
    % "Dict" -> ("Tag", sorted "Pairs")
    % Does NOT fail if Dict is empty. Pairs will just be [].
@@ -67,87 +86,91 @@ dict_pp_inner(Dict,SettingsDict,LinesOut) :-
    %
    % Generate a list of strings "Lines" from the "Pairs" according to "SettingsDict".
    %
-   lineify(Entries,SettingsDict,Lines,[]), % "Lines-[]" is an open difference list, which we close
+   lineify(Entries,SettingsDict,Lines,[]), % "Lines-[]" used to be an open difference list, which we close
    %
    % How to build the output
    %
    decision_how_to_display(Tag,SettingsDict,DecisionForTag,DecisionForBorder,DecisionForPadding),
-   check_that(DecisionForPadding,hard(boolean)),
-   check_that(DecisionForTag,hard(boolean)),
-   check_that(DecisionForBorder,hard(boolean)),
-   dispatch(DecisionForTag,DecisionForBorder,DecisionForPadding,Lines,Tag,SettingsDict,LinesOut).
+   assertion(check_that(DecisionForPadding,hard(boolean))),
+   assertion(check_that(DecisionForTag,hard(boolean))),
+   assertion(check_that(DecisionForBorder,hard(boolean))),
+   once(dispatch(DecisionForTag,DecisionForBorder,DecisionForPadding,Lines,Tag,SettingsDict,ResultLines)).
 
-% dispatch(DecisionForTag,DecisionForBorder,DecisionForPadding,Lines,Tag,SettingsDict,LinesOut)
+% dispatch(DecisionForTag,DecisionForBorder,DecisionForPadding,Lines,Tag,SettingsDict,ResultLines)
 
-dispatch(true  , true  , DecisionForPadding , Lines , Tag , SettingsDict , LinesOut) :- !,add_with_border_with_tag(Lines,Tag,DecisionForPadding,SettingsDict,LinesOut).
-dispatch(false , true  , DecisionForPadding , Lines , _   , SettingsDict , LinesOut) :- !,add_with_border_without_tag(Lines,DecisionForPadding,SettingsDict,LinesOut).
-dispatch(true  , false , true               , Lines , Tag , SettingsDict , LinesOut) :- !,add_with_tag_with_padding(Lines,Tag,SettingsDict,LinesOut).
-dispatch(true  , false , false              , Lines , Tag , SettingsDict , LinesOut) :- !,add_with_tag_without_padding(Lines,Tag,SettingsDict,LinesOut).
-dispatch(false , false , true               , Lines , _   , SettingsDict , LinesOut) :- !,add_with_padding(Lines,SettingsDict,LinesOut).
+dispatch(true  , true  , DecisionForPadding , Lines , Tag , SettingsDict , LinesOut) :- add_with_border_with_tag(Lines,Tag,DecisionForPadding,SettingsDict,LinesOut).
+dispatch(false , true  , DecisionForPadding , Lines , _   , SettingsDict , LinesOut) :- add_with_border_without_tag(Lines,DecisionForPadding,SettingsDict,LinesOut).
+dispatch(true  , false , true               , Lines , Tag , SettingsDict , LinesOut) :- add_with_tag_with_padding(Lines,Tag,SettingsDict,LinesOut).
+dispatch(true  , false , false              , Lines , Tag , SettingsDict , LinesOut) :- add_with_tag_without_padding(Lines,Tag,SettingsDict,LinesOut).
+dispatch(false , false , true               , Lines , _   , SettingsDict , LinesOut) :- add_with_padding(Lines,SettingsDict,LinesOut).
 dispatch(false , false , false              , Lines , _   , _            , Lines).      % no tag, no border, no padding -> just transfer lines
 
-add_with_border_with_tag(Lines,Tag,DecisionForPadding,SettingsDict,LinesOut) :-
+add_with_border_with_tag(Lines,Tag,DecisionForPadding,SettingsDict,ResultLines) :-
    get_padding_settings_clamped(DecisionForPadding,SettingsDict,PadTop,PadBottom,PadLeft,PadRight), % if padding is off, pad at least 0
    max_line_width([Tag|Lines],MaxLineWidth),
    make_tag_line(Tag,MaxLineWidth,PadLeft,PadRight,SettingsDict,TagLine),
    make_bordery_lines(MaxLineWidth,PadLeft,PadRight,BorderLine,BgLine), 
-   LinesOut=[BorderLine|Fin1],
+   ResultLines=[BorderLine|Fin1],
    append_tag_line(TagLine,BgLine,Fin1,Fin2),
    Fin2=[BorderLine|Fin3],
    Pos is 1+PadLeft,
    append_content(Lines,BgLine,Pos,PadTop,PadBottom,Fin3,Fin4),
    Fin4=[BorderLine],
+   assertion(
+      (Len is 1+PadLeft+MaxLineWidth+PadRight+1, 
+      strings_of_given_length(ResultLines,Len))
+   ).
 
-   assertion((Len is 1+PadLeft+MaxLineWidth+PadRight+1, is_all_strings_same_length(LinesOut,Len))).
-
-add_with_border_without_tag(Lines,DecisionForPadding,SettingsDict,LinesOut) :- 
+add_with_border_without_tag(Lines,DecisionForPadding,SettingsDict,ResultLines) :- 
    get_padding_settings_clamped(DecisionForPadding,SettingsDict,PadTop,PadBottom,PadLeft,PadRight), % if padding is off, pad at least 0
    max_line_width(Lines,MaxLineWidth),
    make_bordery_lines(MaxLineWidth,PadLeft,PadRight,BorderLine,BgLine), 
-   LinesOut=[BorderLine|Fin1],
+   ResultLines=[BorderLine|Fin1],
    Pos is 1+PadLeft,
    append_content(Lines,BgLine,Pos,PadTop,PadBottom,Fin1,Fin2),
    Fin2=[BorderLine],
    assertion(
       (Len is 1+PadLeft+MaxLineWidth+PadRight+1, 
-      strings_of_given_length(LinesOut,Len))).
+      strings_of_given_length(ResultLines,Len))
+   ).
 
-add_with_tag_with_padding(Lines,Tag,SettingsDict,LinesOut) :-
+add_with_tag_with_padding(Lines,Tag,SettingsDict,ResultLines) :-
    get_padding_settings(SettingsDict,PadTop,PadBottom,PadLeft,PadRight),
    max_line_width([Tag|Lines],MaxLineWidth),
    make_tag_line(Tag,MaxLineWidth,PadLeft,PadRight,SettingsDict,TagLine),
-   LinesOut=[TagLine|Fin1],
+   ResultLines=[TagLine|Fin1],
    make_background_line_for_padding(MaxLineWidth,PadLeft,PadRight,BgLine),
    append_content(Lines,BgLine,PadLeft,PadTop,PadBottom,Fin1,[]),
    assertion(
       (Len is PadLeft+MaxLineWidth+PadRight,
-      strings_of_given_length(LinesOut,Len))).
+      strings_of_given_length(ResultLines,Len))
+   ).
 
-add_with_tag_without_padding(Lines,Tag,SettingsDict,LinesOut) :-
+add_with_tag_without_padding(Lines,Tag,SettingsDict,ResultLines) :-
    max_line_width([Tag|Lines],MaxLineWidth),
    make_tag_line(Tag,MaxLineWidth,0,0,SettingsDict,TagLine), % has whitespace on the right though
-   LinesOut=[TagLine|Lines]. % variable whitespace on the right
+   ResultLines=[TagLine|Lines]. % variable whitespace on the right
 
-add_with_padding(Lines,SettingsDict,LinesOut) :-
+add_with_padding(Lines,SettingsDict,ResultLines) :-
    get_padding_settings(SettingsDict,PadTop,PadBottom,PadLeft,PadRight),
    max_line_width(Lines,MaxLineWidth),
    make_background_line_for_padding(MaxLineWidth,PadLeft,PadRight,BgLine),
-   append_content(Lines,BgLine,PadLeft,PadTop,PadBottom,LinesOut,[]),
+   append_content(Lines,BgLine,PadLeft,PadTop,PadBottom,ResultLines,[]),
    assertion(
       (Len is PadLeft+MaxLineWidth+PadRight, 
-      strings_of_given_length(LinesOut,Len))).
+      strings_of_given_length(ResultLines,Len))
+   ).
 
 % "it is a list of strings and all the strings have the given length"
-% If List is not a list in the first place, maplist fails (we
-% assume it is not an open list)
+% If List is not a list in the first place, maplist fails (we assume it is not an open list)
 
 strings_of_given_length(List,Length) :-
    maplist({Length}/[X]>>stringy_type_with_length(X,string(Length)),List).
 
 % Append lines underneath the border line underneath the tag
 
-append_content(Lines,BgLine,Pos,PadTop,PadBottom,LinesOut,FinalFin) :-
-   pad_around(PadTop,BgLine,LinesOut,Fin1),                             % append PadTop x BgLine
+append_content(Lines,BgLine,Pos,PadTop,PadBottom,ResultLines,FinalFin) :-
+   pad_around(PadTop,BgLine,ResultLines,Fin1),                          % append PadTop x BgLine
    maplist_onto_open_list(mpl_overwrite(BgLine,Pos),Lines,Fin1,Fin2),   % append content lines 
    pad_around(PadBottom,BgLine,Fin2,FinalFin).                          % append PadBottom x BgLine
 
@@ -156,14 +179,13 @@ append_content(Lines,BgLine,Pos,PadTop,PadBottom,LinesOut,FinalFin) :-
 %  xxxxxxxxxxTAGxxxxxxxx   tag line
 % |                     |  background line with left and right box borders
 
-append_tag_line(TagLine,BgLine,LinesOut,FinalFin) :-
-   maplist_onto_open_list(mpl_overwrite(BgLine,1),[TagLine],LinesOut,FinalFin).
+append_tag_line(TagLine,BgLine,ResultLines,FinalFin) :-
+   maplist_onto_open_list(mpl_overwrite(BgLine,1),[TagLine],ResultLines,FinalFin).
 
-mpl_overwrite(BgLine,Pos,LineIn,LineOut) :-
-   CutLeft=true,CutRight=true, % make clear what's going on
-   stringy_overwrite(BgLine,LineIn,Pos,CutLeft,CutRight,LineOut,string).
-
-% ----------
+mpl_overwrite(BgLine,Pos,LineIn,Result) :-
+   CutLeft=true,
+   CutRight=true,
+   stringy_overwrite(BgLine,LineIn,Pos,CutLeft,CutRight,Result,string).
 
 %! pad_around(+PadCount,+PadLine,?Tip,?FinalFin)
 %
@@ -176,10 +198,4 @@ pad_around(Count,PadLine,[PadLine|NewFin],FinalFin) :-
    assertion(Count>0),
    CountMinus is Count-1,
    pad_around(CountMinus,PadLine,NewFin,FinalFin).
-
-% Check for an assertion: all the strings have the same length
-% If List is not a list, maplist fails.
-
-is_all_strings_same_length(List,Length) :-
-   maplist({Length}/[X]>>(string(X),string_length(X,Length)),List). 
 
